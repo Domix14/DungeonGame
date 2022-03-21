430a73196316066107ada1a373e699328c672b2f
Domix141 <djaworski@student.agh.edu.pl>
2022-03-21 15:32:25 +0100

    Add base Game class

---
+++ include/Game.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+class Game
+{
+public:
+	void Launch();
+
+
+private:
+	void MainLoop();
+
+};
+
---
+++ src/Game.cpp
@@ -0,0 +1,26 @@
+#include "Game.h"
+
+#include <raylib.h>
+
+void Game::Launch()
+{
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "DungeonGame");
+	SetTargetFPS(60);
+	MainLoop();
+}
+
+void Game::MainLoop()
+{
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+}
